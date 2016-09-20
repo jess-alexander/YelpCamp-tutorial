c3d0e6ae73a5fd3f2247976c0d1ff47792c7d666
jess-alexander <alexanderjessmarie@gmail.com>
2016-09-20 11:11:56 +0200

    Update Route
    assign a new variable the value of req.params.id for use in the form
    
    find comment data by id and pass into the form as well.

--- routes/comments.js
+++ routes/comments.js
@@ -47,7 +47,14 @@ router.post("/", isLoggedIn, function(req, res) {
 });
 
 router.get("/:comment_id/edit", function(req, res) {
-    res.send("EDIT ROUTE FOR COMMENTS");
+    Comment.findById(req.params.comment_id, function(err, foundComment) {
+        if (err) {
+            console.log("ERR IN COMMENT EDIT ROUTE");
+            res.redirect("back");
+        } else {
+            res.render("comments/edit", { campground_id: req.params.id, comment: foundComment });
+        }
+    });
 });
 
 
