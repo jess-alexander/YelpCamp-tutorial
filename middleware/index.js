e394772994614e4240f916561515d45414466967
jess-alexander <alexanderjessmarie@gmail.com>
2016-09-21 16:09:01 +0200

    Move all middleware to new middleware directory
    store all functions within an object
    
    require campground and comment models

---
+++ middleware/index.js
@@ -0,0 +1,58 @@
+var Campground = require("../models/campground");
+var Comment = require("../models/comment");
+// all the middleware goes here
+var middlewareObj = {};
+
+
+middlewareObj.isLoggedIn = function(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+};
+
+middlewareObj.checkCampgroundOwnership = function(req, res, next) {
+    if (req.isAuthenticated()) { //is user logged in?
+        Campground.findById(req.params.id, function(err, foundCampground) { //get ALL CAMPGROUND from DB
+            if (err) {
+                console.log("EDIT ROUTE   /campgrounds");
+                console.log(err);
+            } else {
+                if (foundCampground.author.id.equals(req.user.id)) { // does user own the campground
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        res.redirect("back"); //take user to the previous page
+    }
+};
+
+middlewareObj.checkCommentOwnership = function(req, res, next) {
+    console.log("inside checkCommentOwnership");
+    if (req.isAuthenticated()) { //is user logged in?
+        Comment.findById(req.params.comment_id, function(err, foundComment) { //get ALL comment from DB
+            if (err) {
+                console.log("Error: checkCommentOwnership middleware");
+                console.log(err);
+            } else {
+                console.log("no error in finding comment");
+                console.log("foundComment.author.id: " + foundComment.author.id);
+                console.log("req.user.id: " + req.user.id);
+
+                if (foundComment.author.id.equals(req.user.id)) { // does user own the comment
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        res.redirect("back"); //take user to the previous page
+    }
+};
+
+
+module.exports = middlewareObj;
