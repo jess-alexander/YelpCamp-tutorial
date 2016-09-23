fd6e0a9b771491c584c31e63e2f9ea22d4fd7469
jess-alexander <alexanderjessmarie@gmail.com>
2016-09-23 11:48:34 +0200

    Campground/Comment Middleware
    “You need to be logged in to do that”

--- middleware/index.js
+++ middleware/index.js
@@ -8,6 +8,7 @@ middlewareObj.isLoggedIn = function(req, res, next) {
     if (req.isAuthenticated()) {
         return next();
     }
+    req.flash("error", "You need to be logged in to do that."); //"error" is the key, the second argument is the error message
     // the above line doesn't display anything. It will be displayed on the next page (the page we redirect to). 
     // the values passed in will not persist; once they have been displayed once they go away. 
     res.redirect("/login");
@@ -16,13 +17,13 @@ middlewareObj.isLoggedIn = function(req, res, next) {
 middlewareObj.checkCampgroundOwnership = function(req, res, next) {
     if (req.isAuthenticated()) { //is user logged in?
         Campground.findById(req.params.id, function(err, foundCampground) { //get ALL CAMPGROUND from DB
-            if (err) {
-                console.log("error within checkCommentOwnership middleware");
+            if (err) { //error from databse - possibly couldn't find campground, or database error
+                console.log("Error: checkCampgroundOwnership");
                 console.log(err);
                 req.flash("error", "Campground Not Found");
                 res.redirect("back");
             } else {
-                if (foundCampground.author.id.equals(req.user.id)) { // does user own the campground
+                if (foundCampground.author.id.equals(req.user.id)) { // if user owns the campground, proceed. ;) 
                     next();
                 } else {
                     req.flash("error", "You don't have permission to do that");
@@ -30,7 +31,8 @@ middlewareObj.checkCampgroundOwnership = function(req, res, next) {
                 }
             }
         });
-    } else {
+    } else { // user not logged in
+        req.flash("error", "You need to be logged in to do that.");
         res.redirect("back"); //take user to the previous page
     }
 };
