05b8fe1dbf157497ec5bec8af53fa5f9f6cc343b
jess-alexander <alexanderjessmarie@gmail.com>
2016-09-23 12:57:29 +0200

    correct indention

--- models/comment.js
+++ models/comment.js
@@ -1,15 +1,14 @@
-var mongoose    = require("mongoose");
+var mongoose = require("mongoose");
 
 var commentSchema = mongoose.Schema({
-	text: String,
-	author: {
-		id:{
-			type: mongoose.Schema.Types.ObjectId, //see campground, comments object 
-			ref:"User"
-		},
-		username: String
-	} //will eventually be a reference to a user model. Can only do this with a non-relational database
-
+    text: String,
+    author: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId, //see campground, comments object 
+            ref: "User"
+        },
+        username: String
+    },
 });
 
 
